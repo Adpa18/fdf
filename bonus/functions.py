62c27926d6e93f7900c457cbabd4679ab27c29c8
wery <adrien.wery@epitech.eu>
2014-11-23 11:17:10 +0100

    message de commit

--- bonus/functions.py
+++ bonus/functions.py
@@ -9,25 +9,24 @@ def lineas(p1, p2, color, img):
         for x in range(p1.y, p2.y):
             if p1x >= 0 and y >= 0:
                 img[p1.x, x] = color
-                tab.append(sf.Vector2(int(p1x),int(x)))
         for x in range(p2.y, p1.y):
             if p1x >= 0 and y >= 0:
                 img[p1.x, x] = color
-                tab.append(sf.Vector2(int(p1x),int(x)))
-    a = (p2.y-p1.y)/(p2.x-p1.x)
-    b = p1.y - a*p1x
-    while p2.x > p1x:
-        y = a*p1x + b
-        if p1x >= 0 and y >= 0:
-            img[p1x, y] = color
-            tab.append(sf.Vector2(int(p1x),int(y)))
-        p1x = p1x + 0.5
-    while p2.x < p1x:
-        y = a*p1x + b
-        if p1x >= 0 and y >= 0:
-            img[p1x, y] = color
-            tab.append(sf.Vector2(int(p1x),int(y)))
-            p1x = p1x - 0.5
+    else :
+        a = (p2.y-p1.y)/(p2.x-p1.x)
+        b = p1.y - a*p1x
+        while p2.x > p1x:
+            y = a*p1x + b
+            if p1x >= 0 and y >= 0:
+                img[p1x, y] = color
+                tab.append(sf.Vector2(int(p1x),int(y)))
+            p1x = p1x + 0.5
+        while p2.x < p1x:
+            y = a*p1x + b
+            if p1x >= 0 and y >= 0:
+                img[p1x, y] = color
+                tab.append(sf.Vector2(int(p1x),int(y)))
+                p1x = p1x - 0.5
     return (tab)
 
 
